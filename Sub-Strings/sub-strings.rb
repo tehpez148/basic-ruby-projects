907aa571fda009ec48a40a2ea42f78736decabb9
fatman148 <108585064+tehpez148@users.noreply.github.com>
2023-10-13 10:50:05 +0100

    Changed logic to utilise #scan rather than select. Then added results of scan straight to hash instead of dictionary then to a hash

--- Sub-Strings/sub-strings.rb
+++ Sub-Strings/sub-strings.rb
@@ -4,18 +4,40 @@
 
 
 def sub_string (string, dictionary)
-    og_string = string
-    og_array = og_string.split("")
+    og_string = string.downcase
+    og_array = og_string.split(" ")
     p og_array
 
-    new_dict = dictionary.select{|subs| og_string.include? subs}
+    new_hash = {}
 
-    p new_dict
+    dictionary.each do |subs|
+        count = og_string.scan(subs).size
+        if count > 0
+            new_hash[subs] = count
+        end
+    end
+
+    p new_hash
+
+
+
+    ##dictionary.select{|subs| og_string.include? subs}
+
+
+
+    
+    ##result = Hash.new(0)
+    ##new_dict.each { |word| result[word] += 1 }
+    ##p result
+    
+    
+
+
 
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-sub_string("below it howdy", dictionary)
+sub_string("below low", dictionary)
 
     
