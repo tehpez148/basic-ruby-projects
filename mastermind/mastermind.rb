5ce208efaf37347bd89b5e631ba61e3ded4de5fc
fatman148 <108585064+tehpez148@users.noreply.github.com>
2023-10-19 17:00:47 +0100

    Created mastermind file

---
+++ mastermind/mastermind.rb
@@ -0,0 +1,43 @@
+
+class Mastermind
+  @@number_of_guess = 12
+
+    def initialize()
+        puts "Welcome to MasterMind! Select your difficulty (Easy, Medium or Hard)"
+        difficulty = gets.chomp.downcase    
+    end
+    
+    def difficulty_select(difficulty)
+        if difficulty == easy
+            return 0
+        elsif difficulty == medium 
+            return 1
+        elsif difficulty == hard 
+            return 2
+        else 
+            puts "That's not what I asked for! Try again..."
+        end
+    end     
+    
+    def random_numbers
+        r = rand(9)
+    end
+
+    def random_array()
+        rand_array = Array.new
+        i = 4 + difficulty
+        while i != 0
+            rand_array.append(random_numbers)
+            i -= 1
+        end
+        p rand_array
+    end
+
+    
+end
+
+
+new_game = Mastermind.new
+
+
+
