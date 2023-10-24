c2b264e8705e2388bc5883f4d162ee4704e5e099
fatman148 <108585064+tehpez148@users.noreply.github.com>
2023-10-24 17:37:42 +0100

    prints empty tic-tac-toe board

--- tictactoe/tictactoe.rb
+++ tictactoe/tictactoe.rb
@@ -1,22 +1,27 @@
 ## project to create a game of tic-tac-toe
-
+## player will be an object that has the ability to place a marker down. 
 
 
 
 class Game
     def initialize ()
         @board = Array.new(10)
+        puts "Welcome to Tic-Tac-Toe!"
     end
 
-    def display_board(board)
-        puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    def display_board()
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
         puts "-----------"
-        puts " #{board[3]} | #{board[4]} | #{board[5]} "
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
         puts "-----------"
-        puts " #{board[6]} | #{board[7]} | #{board[8]} "
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
     end
+
+
 end
 
 
+
 start = Game.new
+start.display_board()
 
