58a10f68e409928ecee86be725381465479b7d1d
fatman148 <108585064+tehpez148@users.noreply.github.com>
2023-10-26 13:08:01 +0100

    Began to add check win method, hopefully compares winning lines each round to see if a player has reached a winner

--- tictactoe/tictactoe.rb
+++ tictactoe/tictactoe.rb
@@ -3,6 +3,10 @@
 
 
 class Game
+    ## initializes a game objects with a turn counter, a win counter, player scores,
+    ##an array of numbers 1-9
+    ##winning lines
+    LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
     def initialize ()
         @turn = 0
         @win = 0
@@ -16,7 +20,7 @@ class Game
         puts "Enter name of player 2"
         @player2 = gets.chomp
     end
-
+    ##displays array of numbers in a 3x3 board
     def display_board()
         puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
         puts "-----------"
@@ -26,13 +30,11 @@ class Game
     end
 
    
-
+    ##method to check input is valid 
     def check_input(input)
         if (1..9).include?(input) == false
             puts "That's not kosher. Try again!"
         elsif @board[input - 1] == 'X'
-            puts "Input is #{input}"
-            puts "The element as this position is #{@board[input]}"
             puts "That's already been taken! Try again!"
         elsif @board[input - 1] == 'O'
             puts "That's already been taken! Try again!"
@@ -48,8 +50,12 @@ class Game
         until @win == 1
             take_turn()
         end
+        puts "Scores are: "
+        puts"#{@player1}:#{@player_one_score}"
+        puts"#{@player2}:#{@player_two_score}
     end
-    
+    ##take turn method, cycles through players by change value of @turn variable
+    ##changes element in array to X or O as appropriate 
     def take_turn()
         if @turn == 0
             puts "Pick your next move #{@player1}"
@@ -61,6 +67,10 @@ class Game
                 end
             end
             @board.map! {|x| x == input ? 'X': x}
+            if check_win('X')
+                @win = 1
+                puts "#{@player1} has won!"
+                @player_two_score += 1
             @turn = 1
             display_board()
         else @turn == 1
@@ -73,12 +83,23 @@ class Game
                 end
             end
             @board.map! {|x| x == input ? 'O': x}
+            if check_win('O')
+                @win = 1
+                puts "#{@player2} has won!"
+                @player_two_score += 1
             @turn = 0
             display_board()
 
         end
     end
             
+
+    def check_win(marker)
+        LINES.any? do |line|
+            line.all? {|position| @board[position] == marker}
+        end
+    end
+
 
 
 end
